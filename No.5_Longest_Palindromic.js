09370f9f24f6f466fbae6bd397b6716a64abf413
康任均 <kangrenjun@kangrenjundeMacBook-Air.local>
2022-06-30 01:58:11 +0800

    2022/06/30 update02 No.5 js

--- No.5_Longest_Palindromic.js
+++ No.5_Longest_Palindromic.js
@@ -3,5 +3,40 @@
  * @return {string}
  */
  var longestPalindrome = function(s) {
-    
+    let len = s.length;
+    let maxPal = 0;
+    let ansPal = "";
+    let l = 0;
+    let r = 0;
+    for(let i = 0; i < len; i ++)
+    {
+        //odd pal
+        l = i;
+        r = i;
+        while( l >= 0 && r < len && s[r] == s[l])
+        {
+            if((r - l + 1) > maxPal)
+            {
+                ansPal = s.slice(l, r + 1);
+                maxPal = (r - l + 1);
+            }
+            l --;
+            r ++;
+        }
+
+        //even pal
+        l = i;
+        r = i + 1;
+        while( l >= 0 && r < len && s[r] == s[l])
+        {
+            if((r - l + 1) > maxPal)
+            {
+                ansPal = s.slice(l, r + 1);
+                maxPal = (r - l + 1);
+            }
+            l --;
+            r ++;
+        }
+    }
+    return ansPal;
 };
